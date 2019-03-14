ef6aa7955c5fea3900d7ef677523366a3f924576
Jay Fincher <fincher_jay@yahoo.com>
2019-03-14 15:41:05 -0400

    monster game working, fixing the log functionality

--- app.js
+++ app.js
@@ -10,6 +10,56 @@ new Vue({
             this.gameIsRunning = !this.gameIsRunning;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function(){
+            this.monsterHealth -= this.calculateDamage(3, 10);
+                if(this.checkWin()){
+                    return;
+                }
+            this.monsterAttacks();
+        },
+        monsterAttacks: function(){
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        calculateDamage: function(min, max){
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        heal: function(){
+            if(this.playerHealth <=90){
+                this.playerHealth += 10;
+            }
+            this.monsterAttacks();
+        },
+        specialAttack: function(){
+            this.monsterHealth -= this.calculateDamage(10, 20);
+                if(this.checkWin()){
+                    return;
+                }
+            this.monsterAttacks();
+        },
+        calculateDamage: function(min, max){
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        giveUp: function(){
+            this.gameIsRunning = !this.gameIsRunning;
+            this.playerHealth = 0;
+            this.gameResults();
+        },
+        checkWin: function() {
+            if(this.playerHealth <= 0){
+                if(confirm("You lose. Play Again?")){
+                    this.startNewGame();
+                } else {
+                    this.gameIsRunning = !this.gameIsRunning;
+                } return true
+            } else if(this.monsterHealth <=0){
+                if(confirm("You WIN! Play Again?")){
+                    this.startNewGame();
+                } else {
+                    this.gameIsRunning = !this.gameIsRunning;
+                } return true
+            } return false
         }
     }
 })
